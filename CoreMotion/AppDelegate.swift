29bac15a9885acec64f58ac621ce5add754da20a
Ethan Olree <ethanolree@gmail.com>
2022-10-21 23:29:24 -0500

    added new game model

---
+++ CoreMotion/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Marbles
+//
+//  Created by Friedrich Gräter, 2014
+//  Manipulated by Eric Larson, 2015
+//
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+}
---
+++ CoreMotion/ExampleOriginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleOriginViewController.swift
+//  Marbles
+//
+//  Created by Eric Larson on 9/5/18.
+//
+
+import UIKit
+
+class ExampleOriginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
